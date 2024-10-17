bda649944f87d9983aa646f2da49e2a3aab1b25f
tpalmer9 <80932206+tpalmer9@users.noreply.github.com>
2024-10-17 14:02:03 -0700

    Update music.js

--- music.js
+++ music.js
@@ -1,16 +1,1 @@
-// Function to toggle the visibility of sections
-function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section.style.display === "none" || section.style.display === "") {
-        section.style.display = "block"; // Show the section
-    } else {
-        section.style.display = "none"; // Hide the section
-    }
-}
 
-// Ensure sections are hidden when the page loads
-window.onload = function() {
-    document.querySelectorAll('.hidden-section').forEach(function(section) {
-        section.style.display = 'none';
-    });
-};
